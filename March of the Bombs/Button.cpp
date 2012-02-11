854d5f37c2081c200648d93ba3943ed60322efa2
BuffBuff <Cronqvist_Fredrik@hotmail.com>
2012-02-11 17:26:27 +0100

    Added constructors

--- March of the Bombs/Button.cpp
+++ March of the Bombs/Button.cpp
@@ -1,10 +1,24 @@
 #include "Button.h"
 
-Button::Button()
+//constructor without changing the vec4 color
+Button::Button(GLTexture::ptr texture1, GLTexture::ptr texture2)
+	: unpressedTexture(texture1), pressedTexture(texture2), color(glm::vec4(1))
+{
+
+}
+
+//constructor changing the vec4 color
+Button::Button(GLTexture::ptr texture1, GLTexture::ptr texture2, glm::vec4 color)
+	: unpressedTexture(texture1), pressedTexture(texture2), color(color)
 {
 
 }
 
 Button::~Button()
 {
+}
+
+void render()
+{
+
 }
--- March of the Bombs/Button.h
+++ March of the Bombs/Button.h
@@ -2,6 +2,7 @@
 
 #include "GLTexture.h"
 #include <glm\glm.hpp>
+//#include "Rectagle.h"
 
 class Button
 {
@@ -9,8 +10,11 @@ private:
 	GLTexture::ptr unpressedTexture;		//storing buttons unpressedtexture under a name
 	GLTexture::ptr pressedTexture;			//storing buttons pressedtexture under a name
 	glm::vec4 color;						//using vec4 for more options later on, this as color
+	//RECTANGLE
 
 public:
-	Button();
+	Button(GLTexture::ptr texture1, GLTexture::ptr texture2);
+	Button(GLTexture::ptr texture1, GLTexture::ptr texture2, glm::vec4 color);
 	virtual ~Button();
+	void render();
 };
