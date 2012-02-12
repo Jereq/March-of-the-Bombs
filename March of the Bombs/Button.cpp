8640b7dc21be1b5e1cebf0b4195bedb9d0f88eab
BuffBuff <Cronqvist_Fredrik@hotmail.com>
2012-02-12 12:19:56 +0100

    Added simple drawing without any rectangle

--- March of the Bombs/Button.cpp
+++ March of the Bombs/Button.cpp
@@ -1,5 +1,7 @@
 #include "Button.h"
 
+bool pressed = false;
+
 //constructor without changing the vec4 color
 Button::Button(GLTexture::ptr texture1, GLTexture::ptr texture2, Rectangle rectangle)
 	: unpressedTexture(texture1), pressedTexture(texture2), posSizeRectangle(rectangle), color(glm::vec4(1))
@@ -18,7 +20,15 @@ Button::~Button()
 {
 }
 
-void render()
+void Button::render(Graphics::ptr dT)
 {
+	if(!pressed)
+	{
+		dT->drawTexture(unpressedTexture);
+	}
+	else
+	{
+		dT->drawTexture(pressedTexture);
+	}
 
 }
--- March of the Bombs/Button.h
+++ March of the Bombs/Button.h
@@ -2,6 +2,7 @@
 
 #include "GLTexture.h"
 #include "Rectangle.h"
+#include "Graphics.h"
 #include <glm\glm.hpp>
 
 class Button
@@ -11,10 +12,11 @@ private:
 	GLTexture::ptr pressedTexture;			//storing buttons pressedtexture under a name
 	glm::vec4 color;						//using vec4 for more options later on, this as color
 	Rectangle posSizeRectangle;				//position- and size- rectangle 
+	bool pressed;
 
 public:
 	Button(GLTexture::ptr texture1, GLTexture::ptr texture2, Rectangle rectangle);
 	Button(GLTexture::ptr texture1, GLTexture::ptr texture2, Rectangle rectangle, glm::vec4 color);
 	virtual ~Button();
-	void render();
+	void render(Graphics::ptr dT);
 };
