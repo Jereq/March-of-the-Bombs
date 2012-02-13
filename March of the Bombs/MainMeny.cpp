f4628952784602a4906a38f9022b74a5b2653ba2
BuffBuff <Cronqvist_Fredrik@hotmail.com>
2012-02-13 14:13:11 +0100

    Added more images

--- March of the Bombs/MainMeny.cpp
+++ March of the Bombs/MainMeny.cpp
@@ -44,28 +44,33 @@ void MainMeny::draw(Graphics::ptr graphics)
 
 void MainMeny::createButtons()
 {
-	GLTexture::ptr TestButton1 = GLTexture::loadTexture(L"images/skull.tga");
+	//GLTexture::ptr TestButton1 = GLTexture::loadFromFileTGA(L"images/skull.tga");
+	GLTexture::ptr LobbyButton = GLTexture::loadFromFileTGA(L"images/MotBLobby.tga");
+	GLTexture::ptr HtPButton = GLTexture::loadFromFileTGA(L"images/MotBHtP.tga");
+	GLTexture::ptr OptionButton = GLTexture::loadFromFileTGA(L"images/MotBOption.tga");
+	GLTexture::ptr CreditsButton = GLTexture::loadFromFileTGA(L"images/MotBCredits.tga");
+	GLTexture::ptr ExitButton = GLTexture::loadFromFileTGA(L"images/MotBExit.tga");
 
 	//class		name	unpressed		pressed		rectangle	lower left corner	upper right corner
-	Button tempbutton0(TestButton1, TestButton1, Rectanglef(glm::vec2(0.25f,0.42f),glm::vec2(0.50f,0.15f)));
-	Button tempbutton1(TestButton1, TestButton1, Rectanglef(glm::vec2(0.30f,0.32f),glm::vec2(0.40f,0.08f)));
-	Button tempbutton2(TestButton1, TestButton1, Rectanglef(glm::vec2(0.33f,0.22f),glm::vec2(0.35f,0.08f)));
-	Button tempbutton3(TestButton1, TestButton1, Rectanglef(glm::vec2(0.36f,0.12f),glm::vec2(0.30f,0.08f)));
-	Button tempbutton4(TestButton1, TestButton1, Rectanglef(glm::vec2(0.39f,0.02f),glm::vec2(0.25f,0.06f)));
+	Button button0(LobbyButton, LobbyButton, Rectanglef(glm::vec2(0.25f,0.42f),glm::vec2(0.50f,0.15f)));
+	Button button1(HtPButton, HtPButton, Rectanglef(glm::vec2(0.30f,0.32f),glm::vec2(0.40f,0.08f)));
+	Button button2(OptionButton, OptionButton, Rectanglef(glm::vec2(0.33f,0.22f),glm::vec2(0.35f,0.08f)));
+	Button button3(CreditsButton, CreditsButton, Rectanglef(glm::vec2(0.36f,0.12f),glm::vec2(0.30f,0.08f)));
+	Button button4(ExitButton, ExitButton, Rectanglef(glm::vec2(0.39f,0.02f),glm::vec2(0.25f,0.06f)));
 
 
-	buttons.push_back(tempbutton0);
-	buttons.push_back(tempbutton1);
-	buttons.push_back(tempbutton2);
-	buttons.push_back(tempbutton3);
-	buttons.push_back(tempbutton4);
+	buttons.push_back(button0);
+	buttons.push_back(button1);
+	buttons.push_back(button2);
+	buttons.push_back(button3);
+	buttons.push_back(button4);
 }
 
 void MainMeny::createBackground()
 {
-	GLTexture::ptr TestBackground1 = GLTexture::loadTexture(L"images/skull.tga");
+	GLTexture::ptr Background = GLTexture::loadFromFileTGA(L"images/MotBbackground.tga");
 
-	SimpleImage Background1(TestBackground1, Rectanglef(glm::vec2(0.00f,0.00f),glm::vec2(1.00f,1.00f)));
+	SimpleImage Background1(Background, Rectanglef(glm::vec2(0.00f,0.00f),glm::vec2(1.00f,1.00f)));
 
 	Backgrounds.push_back(Background1);
 }
