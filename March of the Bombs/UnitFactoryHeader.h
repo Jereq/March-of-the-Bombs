856f8991c6b19575bd12d126dc472d379b43bfc0
Christoffer Jon <christoffer_jonstrand@hotmail.com>
2012-02-20 18:13:55 +0100

    började på unit factoryn

--- March of the Bombs/UnitFactoryHeader.h
+++ March of the Bombs/UnitFactoryHeader.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <iostream>
+//http://login2win.blogspot.com/2008/05/c-factory-pattern.html
 
 class UnitFactory
 {
@@ -7,4 +9,10 @@ private:
 	void makeFastBomb();
 	void makeClusterBomb();
 	void makeDud();
-}
+};
+class Bomb 
+{
+public:
+	virtual int bombType() = 0;
+	virtual int team() = 0;
+};
