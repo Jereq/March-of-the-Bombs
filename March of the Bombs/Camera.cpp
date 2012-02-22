c4b3b1d7416a471b18a3224bf380b39b8eca7344
Sebastian Larsson <sjereq@gmail.com>
2012-02-22 23:30:57 +0100

    Changed back to using glm's own perspective and lookAt functions
    My versions was shameless copies after all.

--- March of the Bombs/Camera.cpp
+++ March of the Bombs/Camera.cpp
@@ -10,55 +10,13 @@ Camera::Camera(AttachmentPoint::ptr attachmentPoint)
 	updateViewMatrix();
 }
 
-glm::mat4 perspective(float feildOfViewY_deg, float aspectRatio, float nearClip, float farClip)
-{
-	float range = tan(glm::radians(feildOfViewY_deg / 2)) * nearClip;	
-	float left = -range * aspectRatio;
-	float right = range * aspectRatio;
-	float bottom = -range;
-	float top = range;
-
-	glm::mat4 Result(0);
-	Result[0][0] = (2 * nearClip) / (right - left);
-	Result[1][1] = (2 * nearClip) / (top - bottom);
-	Result[2][2] = - (farClip + nearClip) / (farClip - nearClip);
-	Result[2][3] = - 1;
-	Result[3][2] = - (2 * farClip * nearClip) / (farClip - nearClip);
-	return Result;
-}
-
 void Camera::updateProjectionMatrix(float aspect)
 {
 	const float nearClip = 0.1f;
 	const float farClip = 10000.f;
 	const float fieldOfViewY_deg = 45.f;
 
-	projectionMatrix = perspective(fieldOfViewY_deg, aspect, nearClip, farClip);
-}
-
-glm::mat4 lookAt(vec3 position, vec3 target, vec3 up)
-{
-	vec3 f = glm::normalize(target - position);
-	vec3 u = glm::normalize(up);
-	vec3 s = glm::normalize(glm::cross(f, u));
-	u = glm::cross(s, f);
-
-	glm::mat4 Result(1);
-	Result[0][0] = s.x;
-	Result[1][0] = s.y;
-	Result[2][0] = s.z;
-	Result[0][1] = u.x;
-	Result[1][1] = u.y;
-	Result[2][1] = u.z;
-	Result[0][2] =-f.x;
-	Result[1][2] =-f.y;
-	Result[2][2] =-f.z;
-/*  Test this instead of translate3D
-	Result[3][0] =-dot(s, eye);
-	Result[3][1] =-dot(y, eye);
-	Result[3][2] = dot(f, eye);
-*/  
-	return glm::translate(Result, -position);
+	projectionMatrix = glm::perspective(fieldOfViewY_deg, aspect, nearClip, farClip);
 }
 
 void Camera::updateViewMatrix()
@@ -73,7 +31,7 @@ void Camera::updateViewMatrix()
 	glm::vec3 forwardDirection = rotateY * rotateX * glm::vec3(0, 0, -1);
 	glm::vec3 rightDirection = rotateY * glm::vec3(1, 0, 0);
 
-	viewMatrix = lookAt(position, position + forwardDirection, glm::cross(rightDirection, forwardDirection));
+	viewMatrix = glm::lookAt(position, position + forwardDirection, glm::cross(rightDirection, forwardDirection));
 }
 
 glm::mat4 Camera::getProjectionMatrix() const
