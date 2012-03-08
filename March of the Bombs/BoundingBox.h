36c309b278f8a1e263f15866f8f93504942585e6
Zakallamo <johnylof@hotmail.com>
2012-03-08 13:07:54 +0100

    Revert "commented it out for testing purpose"
    This reverts commit 553ad69805fca9e0c50520c0d467e73a082a688e.
    
    Conflicts:
    
    	March of the Bombs/BoundingBox.cpp
    	March of the Bombs/BoundingBox.h
    	March of the Bombs/March of the Bombs.vcxproj.filters

--- March of the Bombs/BoundingBox.h
+++ March of the Bombs/BoundingBox.h
@@ -11,8 +11,4 @@ public:
 	BoundingBox(glm::vec3 const& position, glm::vec3 const& halfSize);
 
 	bool rayIntersect(glm::vec3 origin, glm::vec3 direction, float& distance) const;
-
-	//// TLN = TopLeftNear, BRN = BottomRightNear
-	//// TLF = TopLeftFar, BRF = BottomRightFar
-	//void selectionBox(glm::vec3 TLN, glm::vec3 BRN, glm::vec3 TLF, glm::vec3 BRF, BoundingBox& BoundingBox);
 };
